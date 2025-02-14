856fdfe539e2fc0947e9c4e7051d89d4eb561bef
ip48 <innapolak@gmail.com>
2025-02-14 17:29:21 -0500

    task 6

--- server.py
+++ server.py
@@ -4,8 +4,10 @@
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
 # Import the sentiment_analyzer function from the package created: TODO
-
-#Initiate the flask app : TODO
+from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+#Initiate the flask app
+app = Flask("Inna Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
@@ -14,15 +16,22 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
+    textToAnalyze = request.args["textToAnalyze"]
+    res = sentiment_analysis(textToAnalyze)
+    score = res["score"]
+    sentiment = res["label"].split('_')[1]
+    return f"The given text has been identified as {sentiment} with a score of {score}"
+    
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
+    return render_template("index.html")
+   
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    '''
+    app.run(debug = True)
